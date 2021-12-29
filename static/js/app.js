f62f05fbd0c6609f0fb20c3cc6714aaec6e63d02
MahooBox <mauricio@nebra.co>
2021-12-29 10:42:54 -0500

    individual phrases

--- static/js/app.js
+++ static/js/app.js
@@ -6,8 +6,18 @@ let url = 'https://api.sampleapis.com/futurama/characters/'
         
     const mostrarData = (data) => {
         console.log(data)
+
+        function individuales(info){
+            let parrafo = ''
+            for (let m = 0; m<info.length; m++){
+                parrafo += `<p>${info[m]}</p>`                
+            }
+            return(document.getElementById('data').innerHTML = parrafo)
+        }
+
         let body = ''
         for (let i = 0; i<data.length; i++){
+            
             body += `<tr>
                 <td>${data[i].name.first} ${data[i].name.middle} ${data[i].name.last}</td>
                 <td>${data[i].gender}</td>
@@ -16,24 +26,27 @@ let url = 'https://api.sampleapis.com/futurama/characters/'
                 <td>${data[i].occupation}</td>
                 <td><img src="${data[i].images.main}"/></td>
                 <td>${data[i].sayings}</td>
+                <td>
+                    <p> ${individuales(data[i].sayings)}</p>
+                </td>
             </tr>`                
         }
         document.getElementById('data').innerHTML = body
     }
 
 
-let url_individual = 'https://api.sampleapis.com/futurama/characters/1'
-    fetch(url_individual)
-        .then(response => response.json())
-        .then(datos => mostrarDatos(datos))
-        .catch(error => console.log(error))
+// let url_individual = 'https://api.sampleapis.com/futurama/characters/1'
+//     fetch(url_individual)
+//         .then(response => response.json())
+//         .then(datos => mostrarDatos(datos))
+//         .catch(error => console.log(error))
         
-    const mostrarDatos = (datos) => {
-        let info = datos.sayings
-        console.log(info)
-        let parrafo = ''
-        for (let recorra = 0; recorra<info.length; recorra++){
-            parrafo += `<p>${info[recorra]}</p>`                
-        }
-        document.getElementById('frases').innerHTML = parrafo
-    }
+//     const mostrarDatos = (datos) => {
+//         let info = datos.sayings
+//         //console.log(info)
+//         let parrafo = ''
+//         for (let m = 0; m<info.length; m++){
+//             parrafo += `<p>${info[m]}</p>`                
+//         }
+//         document.getElementById('frases').innerHTML = parrafo
+//     }
